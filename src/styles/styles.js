3e5a14df1710b984e93ad27ed8008d35b9074063
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-10-20 21:38:53 +0200

    styles

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -4,18 +4,19 @@ import { rhythm } from "../utils/typography"
 export const Wrapper = styled.div`
   margin: auto;
   max-width: 1700px;
-  height: 1000px;
+  height: 15000px;
   padding: ${rhythm(1.5)};
   padding-top: ${rhythm(1.5)};
   background-image: url("/images/bg.jpg");
 
   background-color: ${({ theme }) =>
-    theme === "light" ? "#f2eecb" : "#000"}; // #f2eecb old paper
+    theme === "light" ? "#f2eecb" : "#121212"}; // #f2eecb old paper
 
   h3,
   h4,
   p {
     color: ${({ theme }) => (theme === "dark" ? "#F0FFFF" : "#212121")};
+    text-emphasis: ${({ theme }) => (theme === "dark" ? "87%" : "100%")};
   }
 
   h5 {
@@ -24,5 +25,9 @@ export const Wrapper = styled.div`
 
   p {
     font-size: 12px;
+  }
+
+  button {
+    color: ${({ theme }) => (theme === "dark" ? "#e07b39" : "#1979a9")};
   }
 `
