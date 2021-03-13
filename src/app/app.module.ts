03c96bee1772aa365674c2fc3c76ae40b0b6b9d2
Kateryna Bugaieva <sonnensmile@gmail.com>
2021-03-13 15:32:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule,
